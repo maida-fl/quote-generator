1019fc8645c0ec5a67cbf7ec4e468bb09da97a12
maida-fl <maida.guzman@outlook.com>
2021-11-29 13:45:34 -0300

    loader added

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,25 @@ const quoteText = document.querySelector('#quote');
 const quoteAuthor = document.querySelector('#author');
 const twitterBtn = document.querySelector('#twitter');
 const newQuoteBtn = document.querySelector('#new-quote');
+const loader = document.querySelector('#loader');
+
+const loading = function() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+const complete = function() {
+    if(!loader.hidden) {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+    }
+}
 
 const getQuote = function(){
+    loading();
     const proxyUrl = "https://mighty-lowlands-02831.herokuapp.com/" ;
     const apiQuotes = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+    // Get quote
     fetch(proxyUrl + apiQuotes)
         .then(function(response){
             return response.json();
@@ -27,6 +42,8 @@ const getQuote = function(){
                 quoteText.classList.remove('long-quote');                
             }
             quoteText.innerText = data.quoteText;
+            // Stop Loader & show quote
+            complete();
         })
         .catch(function(error){
             getQuote();
@@ -34,6 +51,15 @@ const getQuote = function(){
         })
 };
 
-
 window.addEventListener("load", getQuote);
+
+twitterBtn.addEventListener("click", function(){
+    const quote = quoteText.innerText;
+    const author = quoteAuthor.innerText;
+    const twitterUrl = "https://twitter.com/intent/tweet?text=" + quote + " - " + author;
+    // const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`; 
+    window.open(twitterUrl, "_blank");
+})
+
+newQuoteBtn.addEventListener("click", getQuote);
 
