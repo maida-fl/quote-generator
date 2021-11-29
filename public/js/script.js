80975a9157a04c9a6456047985e82aea6695286c
maida-fl <maida.guzman@outlook.com>
2021-11-28 22:37:28 -0300

    reduce fontsize for long quotes

--- public/js/script.js
+++ public/js/script.js
@@ -4,7 +4,7 @@ const quoteAuthor = document.querySelector('#author');
 const twitterBtn = document.querySelector('#twitter');
 const newQuoteBtn = document.querySelector('#new-quote');
 
-window.addEventListener("load", function getQuote(){
+const getQuote = function(){
     const proxyUrl = "https://mighty-lowlands-02831.herokuapp.com/" ;
     const apiQuotes = "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
     fetch(proxyUrl + apiQuotes)
@@ -13,11 +13,27 @@ window.addEventListener("load", function getQuote(){
         })
         .then(function(data){
             console.log(data);
-            quoteAuthor.innerHTML = data.quoteAuthor;  
-            quoteText.innerHTML = data.quoteText;
+
+            // Show "unknown" for quotes without author
+            if (data.quoteAuthor === "") {
+                quoteAuthor.innerText = "Unkown";                
+            } else {
+                quoteAuthor.innerText = data.quoteAuthor;                
+            }
+            // Reduce font-size for long quotes
+            if (data.quoteText.length > 90) {
+                quoteText.classList.add('long-quote');
+            } else {
+                quoteText.classList.remove('long-quote');                
+            }
+            quoteText.innerText = data.quoteText;
         })
         .catch(function(error){
             getQuote();
             console.log("Looks like there's a problem with this quote", error);
         })
-});
+};
+
+
+window.addEventListener("load", getQuote);
+
